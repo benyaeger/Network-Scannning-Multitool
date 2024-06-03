ad0553f9d45ad177eaa57f591ba8a309ce91eb87
benyaeger <benyaeger13@gmail.com>
2024-06-03 22:56:44 +0300

    V1.03
    hosts OS detection implemented using nmap host testing via responses analysis. Also doubled the ARP packets to deal with packet loss and raise chances of initial detection

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
-from scapy.layers.inet import IP, TCP, UDP, sr1, ICMP, sr, Ether
+from scapy.layers.inet import Ether
 from scapy.layers.l2 import ARP
 from scapy.sendrecv import sendp
 from scapy.all import sniff
-import psutil
-
-
-def get_subnet_mask(interface_type):
-    try:
-        # get all network interfaces on machine
-        net_if_addrs = psutil.net_if_addrs()
-        # If desired interface type exists
-        if interface_type in net_if_addrs:
-            for addr in net_if_addrs[interface_type]:
-                # addr.family value of 2 is IPv4
-                if addr.family == 2:
-                    # return subnet mask of interface
-                    return addr.netmask
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+import nmap
+from socket import *
 
 
 def lan_scan():
     packets = []
     for mask in range(1, 255):
         dst = '10.0.0.' + str(mask)
+        if dst == gethostbyname(gethostname()):
+            continue
         p = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=dst)
+        packets.append(p)
         packets.append(p)
     sendp(packets)
     answer_packets = sniff(timeout=5, filter="arp")  # Capture ARP reply packets with op code 2 (reply)
-    print(answer_packets)
+
+    online_hosts = set()
     for answer in answer_packets:
-        print('{} responded and is Online.'.format(answer.psrc))
+        online_hosts.add(answer.psrc)
+
+    print('{} hosts found in the LAN: {}'.format(len(online_hosts), online_hosts))
+    should_continue = True if input('Scan for OS Details? (Y/N)') == 'Y' else False
+
+    online_hosts = list(online_hosts)
+    if should_continue:
+        nm = nmap.PortScanner()
+        for host in online_hosts:
+            print('Analyzing {}...'.format(host))
+            try:
+                nm.scan(host, arguments='-O', timeout=15)
+            except nmap.nmap.PortScannerTimeout:
+                print('OS Detection Timed Out')
+                continue
+            try:
+                os_detection = nm[host]['osmatch'][0]['name']
+                os_detection_accuracy = nm[host]['osmatch'][0]['accuracy']
+                print('{} runs {} ({}% Accuracy)'.format(host, os_detection, os_detection_accuracy))
+            except KeyError:
+                print('OS Detection Failed')
+                print(nm)
+    else:
+        pass
 
 
 if __name__ == '__main__':
