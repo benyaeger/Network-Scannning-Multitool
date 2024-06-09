72c96ccf88b360c250b39f46232852a5dd1d637b
benyaeger <benyaeger13@gmail.com>
2024-06-09 18:25:43 +0300

    V 1.04
    Added Port Scanning Operation. Allowing the user to interact with different mechanism. Added requirements.txt file for easy package installation

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from scapy.layers.inet import Ether
+from socket import *
+import nmap
+from scapy.all import sniff
+from scapy.layers.inet import Ether, IP, sr, TCP
 from scapy.layers.l2 import ARP
 from scapy.sendrecv import sendp
-from scapy.all import sniff
-import nmap
-from socket import *
+
+
+def intro_print():
+    print('*' * 50)
+    print("Welcome to Ben's Network Multitool")
+    print('*' * 50)
 
 
 def lan_scan():
+    print("Scanning Network for Online Hosts...")
     packets = []
     for mask in range(1, 255):
         dst = '10.0.0.' + str(mask)
@@ -15,7 +22,8 @@ def lan_scan():
         p = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=dst)
         packets.append(p)
         packets.append(p)
-    sendp(packets)
+        packets.append(p)
+    sendp(packets, verbose=0)
     answer_packets = sniff(timeout=5, filter="arp")  # Capture ARP reply packets with op code 2 (reply)
 
     online_hosts = set()
@@ -23,28 +31,64 @@ def lan_scan():
         online_hosts.add(answer.psrc)
 
     print('{} hosts found in the LAN: {}'.format(len(online_hosts), online_hosts))
-    should_continue = True if input('Scan for OS Details? (Y/N)') == 'Y' else False
 
-    online_hosts = list(online_hosts)
-    if should_continue:
-        nm = nmap.PortScanner()
-        for host in online_hosts:
-            print('Analyzing {}...'.format(host))
+
+def os_detect():
+    host = input("Enter Target Host: ")
+    print("Performing OS Scan...")
+    nm = nmap.PortScanner()
+    try:
+        nm.scan(host, arguments='-O', timeout=15)
+    except nmap.nmap.PortScannerTimeout:
+        print('OS Detection Timed Out')
+    try:
+        os_detection = nm[host]['osmatch'][0]['name']
+        os_detection_accuracy = nm[host]['osmatch'][0]['accuracy']
+        print('{} runs {} ({}% Accuracy)'.format(host, os_detection, os_detection_accuracy))
+    except KeyError:
+        print('OS Detection Failed')
+
+
+def port_scan():
+    host = input("Enter Target Host: ")
+    ans, _ = sr(IP(dst=host) / TCP(dport=(0, 1023), flags='S'), verbose=0, timeout=5)
+    openports = []
+    for sent, received in ans:
+        if received.haslayer(TCP) and received.haslayer(IP):
+            tcp_layer = received.getlayer(TCP)
+        else:
+            continue
+        if tcp_layer.flags == 'SA':
+            openports.append(received)
             try:
-                nm.scan(host, arguments='-O', timeout=15)
-            except nmap.nmap.PortScannerTimeout:
-                print('OS Detection Timed Out')
-                continue
-            try:
-                os_detection = nm[host]['osmatch'][0]['name']
-                os_detection_accuracy = nm[host]['osmatch'][0]['accuracy']
-                print('{} runs {} ({}% Accuracy)'.format(host, os_detection, os_detection_accuracy))
-            except KeyError:
-                print('OS Detection Failed')
-                print(nm)
-    else:
-        pass
+                service = getservbyport(tcp_layer.sport)
+            except OSError:
+                service = 'unknown'
+            print("port {} ({}) is open".format(tcp_layer.sport, service))
+    if len(openports) == 0: print("No Open Ports was Found on host {}".format(host))
+
+
+def esc():
+    exit()
+
+
+operations = {
+    1: lan_scan,
+    2: os_detect,
+    3: port_scan,
+    4: esc
+}
+
+
+def tool_picker():
+    print("What would you like to perform?")
+    for key in operations:
+        print("({}) {}".format(key,operations[key].__name__))
+    picked_operation = input()
+    operations[int(picked_operation)]()
 
 
 if __name__ == '__main__':
-    lan_scan()
+    intro_print()
+    while True:
+        tool_picker()
